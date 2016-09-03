4da845465d199ce97912a7cbd05f8d35a0df7daa
rosston <ross.brandes@gmail.com>
2016-09-03 11:17:22 -0400

    Add basic project files.
    Just leiningen-generated project with slight tweaks.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject doll-smuggler "0.1.0-SNAPSHOT"
+  :description "Solves the problem at https://github.com/micahalles/doll-smuggler"
+  :url "https://bitbucket.org/rosston/doll-smuggler"
+  :license {:name "Unlicense"
+            :url "http://unlicense.org/"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
