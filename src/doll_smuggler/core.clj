f36593350eeafd6a6bd0657fd99e6ea1de345b4e
rosston <ross.brandes@gmail.com>
2016-09-03 19:46:37 -0400

    Add initial working function.
    Uses (somewhat unsafe) recursion.

--- src/doll_smuggler/core.clj
+++ src/doll_smuggler/core.clj
@@ -1,6 +1,18 @@
 (ns doll-smuggler.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defrecord Doll [name weight value])
+
+(defn fill-handbag
+  "Return the set of dolls that maximizes value while fitting within specified weight."
+  [max-weight dolls]
+  (case (count dolls)
+    0 []
+    1 (if (<= (:weight (first dolls)) max-weight) dolls [])
+    (let [sorted-dolls (reverse (sort-by :value dolls))
+          first-doll (first sorted-dolls)
+          first-doll-weight (:weight first-doll)]
+      (if (<= first-doll-weight max-weight)
+        (into [] (concat [first-doll]
+                         (fill-handbag (- max-weight first-doll-weight)
+                                       (rest sorted-dolls))))
+        (fill-handbag max-weight (rest sorted-dolls))))))
--- test/doll_smuggler/core_test.clj
+++ test/doll_smuggler/core_test.clj
@@ -2,6 +2,78 @@
   (:require [clojure.test :refer :all]
             [doll-smuggler.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn sort-by-value
+  "Sort a doll-containing collection by value, descending"
+  [dolls]
+  (reverse (sort-by (juxt :value :weight :name) dolls)))
+
+(defn equal-enough?
+  "Determine if two or more doll-containing collections have the same contents, ignoring order"
+  [& collections]
+  (or (apply = collections)
+      (and (apply = (map type collections))
+           (apply = (map sort-by-value collections)))))
+
+(deftest fill-handbag-test
+  (testing "fill-handbag"
+    (testing "should return provided dolls when total weight of dolls is less than max-weight"
+      (let [dolls [(->Doll "luke" 9 150)
+                   (->Doll "anthony" 13 35)]]
+        (is (= (fill-handbag 23 dolls) dolls))))
+    (testing "should return provided dolls when total weight of dolls is equal to max-weight"
+      (let [dolls [(->Doll "luke" 9 150)
+                   (->Doll "anthony" 13 35)]]
+        (is (= (fill-handbag 22 dolls) dolls))))
+    (testing "should return empty vector when no dolls are provided"
+      (is (= (fill-handbag 400 []) [])))
+    (testing "should return empty vector when one doll is provided but weighs more than max-weight"
+      (is (= (fill-handbag 3 [(->Doll "luke" 9 150)]) [])))
+    (testing "should return vector containgin highest value doll when two dolls are provided but one weighs more than max-weight"
+      (let [dolls [(->Doll "anthony" 13 35)
+                   (->Doll "luke" 9 150)]]
+        (is (= (fill-handbag 10 dolls) [(->Doll "luke" 9 150)]))))
+    (testing "should return empty vector when no single doll weighs less than max-weight"
+      (let [dolls [(->Doll "luke" 9 150)
+                   (->Doll "anthony" 13 35)]]
+        (is (= (fill-handbag 8 dolls) []))))
+    (testing "should return most valuable set of dolls in simple case"
+      (let [dolls [(->Doll "luke" 9 150)
+                   (->Doll "anthony" 13 35)
+                   (->Doll "candice" 153 200)]]
+        (is (equal-enough? (fill-handbag 166 dolls) [(->Doll "luke" 9 150)
+                                                     (->Doll "candice" 153 200)]))))
+    (testing "should return most valuable set of dolls in complex case (example test)"
+      (is (equal-enough? (fill-handbag 400 [(->Doll "luke" 9 150)
+                                            (->Doll "anthony" 13 35)
+                                            (->Doll "candice" 153 200)
+                                            (->Doll "dorothy" 50 160)
+                                            (->Doll "puppy" 15 60)
+                                            (->Doll "thomas" 68 45)
+                                            (->Doll "randal" 27 60)
+                                            (->Doll "april" 39 40)
+                                            (->Doll "nancy" 23 30)
+                                            (->Doll "bonnie" 52 10)
+                                            (->Doll "marc" 11 70)
+                                            (->Doll "kate" 32 30)
+                                            (->Doll "tbone" 24 15)
+                                            (->Doll "tommy" 48 10)
+                                            (->Doll "uma" 73 40)
+                                            (->Doll "grumpkin" 42 70)
+                                            (->Doll "dusty" 43 75)
+                                            (->Doll "grumpy" 22 80)
+                                            (->Doll "eddie" 7 20)
+                                            (->Doll "tory" 18 12)
+                                            (->Doll "sally" 4 50)
+                                            (->Doll "babe" 30 10)])
+                         [(->Doll "sally" 4 50)
+                          (->Doll "eddie" 7 20)
+                          (->Doll "grumpy" 22 80)
+                          (->Doll "dusty" 43 75)
+                          (->Doll "grumpkin" 42 70)
+                          (->Doll "marc" 11 70)
+                          (->Doll "randal" 27 60)
+                          (->Doll "puppy" 15 60)
+                          (->Doll "dorothy" 50 160)
+                          (->Doll "candice" 153 200)
+                          (->Doll "anthony" 13 35)
+                          (->Doll "luke" 9 150)])))))
